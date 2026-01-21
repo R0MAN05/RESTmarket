3a02674a904f7a99954debfd4a3ce903bcb9c808
roman <adhikariroman01m@gmail.com>
2026-01-21 20:31:04 +0545

    Frontend setup with necessary npm packages and ChakraUI.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router";
+
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route index="/" element={<HomePage />}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
