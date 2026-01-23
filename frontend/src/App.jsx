9ad6fe150945ebd91d8725f058772dce38ae5fae
roman <adhikariroman01m@gmail.com>
2026-01-23 16:18:20 +0545

    refactor: migrate from Chakra UI to Tailwind CSS and DaisyUI
    - Updated package.json to remove Chakra UI dependencies and add Tailwind CSS and DaisyUI.
    - Replaced Chakra UI components with Tailwind CSS styles in App.jsx and added a new NavBar component.
    - Created a new CreatePage component for future development.
    - Removed unused color mode, provider, toaster, and tooltip components related to Chakra UI.
    - Added global styles for Tailwind CSS in index.css.
    - Updated Vite configuration to include Tailwind CSS plugin.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import { Routes, Route } from "react-router";
 
+import NavBar from "./pageComponents/NavBar";
+
 import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
 
 function App() {
   return (
-    <>
+    <div style={{ maxHeight: "100vh" }}>
+      <NavBar />
       <Routes>
         <Route index="/" element={<HomePage />}></Route>
+        <Route index="/create" element={<CreatePage />}></Route>
       </Routes>
-    </>
+    </div>
   );
 }
 
